a9be1b7be3f582577ee32b99fd9d5bf5eec053a9
Hooray&T_Tian <32288333+hooray1998@users.noreply.github.com>
2020-01-28 14:48:51 +0800

    加速度传感器计步器(记录最大最小加速度的模)

--- main/java/com/example/myapplication3/MainActivity.java
+++ main/java/com/example/myapplication3/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication3;
 
-import com.example.myapplication3.compassView.CompassView;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,6 +18,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -30,47 +30,112 @@ import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
 
-    private TextView tv_value1;
-    private TextView tv_value2;
-    private TextView tv_value3;
     private SensorManager sManager;
-    private Sensor mSensorOrientation;
-    private CompassView cView;
+    private Sensor mSensorAccelerometer;
+    private TextView tv_step;
+    private TextView tv_mo;
+    private TextView tv_min;
+    private TextView tv_max;
+    private Button btn_start;
+    private int step = 0;   //步数
+    private double oriValue = 0;  //原始值
+    private double lstValue = 0;  //上次的值
+    private double curValue = 0;  //当前值
+    private double minValue = 100;  //当前值
+    private double maxValue = 0;  //当前值
+    private boolean motiveState = true;   //是否处于运动状态
+    private boolean processState = false;   //标记当前是否已经在计步
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cView = new CompassView(MainActivity.this);
+        setContentView(R.layout.activity_main);
         sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mSensorOrientation = sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        sManager.registerListener(this, mSensorOrientation, SensorManager.SENSOR_DELAY_UI);
-        //bindViews();
-        setContentView(cView);
+        mSensorAccelerometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        bindViews();
     }
 
     private void bindViews() {
-        tv_value1 = (TextView) findViewById(R.id.tv_value1);
-        tv_value2 = (TextView) findViewById(R.id.tv_value2);
-        tv_value3 = (TextView) findViewById(R.id.tv_value3);
-        cView = (CompassView) findViewById(R.id.v_compassView);
+
+        tv_step = (TextView) findViewById(R.id.tv_step);
+        tv_mo = (TextView) findViewById(R.id.tv_mo);
+        tv_min = (TextView) findViewById(R.id.tv_min);
+        tv_max = (TextView) findViewById(R.id.tv_max);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        cView.setDegree(event.values[0]);
-        //tv_value1.setText("方位角：" + (float) (Math.round(event.values[0] * 100)) / 100);
-        //tv_value2.setText("倾斜角：" + (float) (Math.round(event.values[1] * 100)) / 100);
-        //tv_value3.setText("滚动角：" + (float) (Math.round(event.values[2] * 100)) / 100);
+        double range = 5;   //设定一个精度范围
+        float[] value = event.values;
+        curValue = magnitude(value[0], value[1], value[2]);   //计算当前的模
+        tv_mo.setText((float)Math.round(curValue*10)/10+"");
+        if(curValue>maxValue){
+            maxValue = curValue;
+            tv_max.setText((float)Math.round(maxValue*10)/10+"");
+        }
+        if(curValue<minValue){
+            minValue = curValue;
+            tv_min.setText((float)Math.round(minValue*10)/10+"");
+        }
+        //向上加速的状态
+        if (motiveState == true) {
+            if (curValue >= lstValue) lstValue = curValue;
+            else {
+                //检测到一次峰值
+                if (Math.abs(curValue - lstValue) > range) {
+                    oriValue = curValue;
+                    motiveState = false;
+                }
+            }
+        }
+        //向下加速的状态
+        if (motiveState == false) {
+            if (curValue <= lstValue) lstValue = curValue;
+            else {
+                if (Math.abs(curValue - lstValue) > range) {
+                    //检测到一次峰值
+                    oriValue = curValue;
+                    if (processState == true) {
+                        step++;  //步数 + 1
+                        if (processState == true) {
+                            tv_step.setText(step + "");    //读数更新
+                        }
+                    }
+                    motiveState = true;
+                }
+            }
+        }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
+    @Override
+    public void onClick(View v) {
+        step = 0;
+        tv_step.setText("0");
+        if (processState == true) {
+            btn_start.setText("开始");
+            processState = false;
+        } else {
+            btn_start.setText("停止");
+            processState = true;
+        }
     }
 
-
+    //向量求模
+    public double magnitude(float x, float y, float z) {
+        double magnitude = 0;
+        magnitude = Math.sqrt(x * x + y * y + z * z);
+        return magnitude;
+    }
 
     @Override
     protected void onDestroy() {
@@ -78,4 +143,3 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sManager.unregisterListener(this);
     }
 }
-
